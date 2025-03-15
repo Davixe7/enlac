8f04f84009a0b9fff34364295b47bae1b2a78fdd
davixe7 <davidguilarte7@gmail.com>
2025-03-15 02:43:19 -0400

    okis

--- database/migrations/2025_02_17_035418_create_candidates_table.php
+++ database/migrations/2025_02_17_035418_create_candidates_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
             $table->string('info_channel');
             $table->boolean('acceptance_status')->default(null)->nullable();
             $table->string('rejection_comment')->default(null)->nullable();
+            $table->date('onboard_at')->default(null)->nullable();
             $table->foreignId('program_id')->nullable()->constrained()->onDelete('cascade');
             $table->timestamps();
         });
