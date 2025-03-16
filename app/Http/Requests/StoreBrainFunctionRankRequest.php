1e82d5db0ab8b05578c007eb071bc964d59a01c4
davixe7 <davidguilarte7@gmail.com>
2025-03-16 00:50:34 -0400

    laterality_impact is nullable now, added required_if validation rules and message to comments and laterality impact fields in BrainFunctionRank form request if caracteristic is 0 o F

--- app/Http/Requests/StoreBrainFunctionRankRequest.php
+++ app/Http/Requests/StoreBrainFunctionRankRequest.php
@@ -22,23 +22,24 @@ class StoreBrainFunctionRankRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'caracteristic' => 'required|in:0,F,P',
-            'comments' => 'nullable|string',
-            'laterality_impact' => 'required|in:l,r',
-            'brain_level_id' => 'required|exists:brain_levels,id',
+            'caracteristic'     => 'required|in:0,F,P',
+            'comments'          => 'nullable|string|required_if:caracteristic,0,F|max:500',
+            'laterality_impact' => 'nullable|in:l,r|required_if:caracteristic,0,F',
+            'brain_level_id'    => 'required|exists:brain_levels,id',
             'brain_function_id' => 'required|exists:brain_functions,id',
-            'candidate_id' => 'required|exists:candidates,id',
+            'candidate_id'      => 'required|exists:candidates,id',
         ];
     }
 
     public function messages(): array
     {
         return [
-            'caracteristic.required' => 'La característica es requerida.',
-            'caracteristic.in' => 'La característica debe ser 0, F o P.',
-            'comments.string' => 'Los comentarios deben ser una cadena de texto.',
-            'laterality_impact.required' => 'El impacto de la lateralidad es requerido.',
-            'laterality_impact.in' => 'El impacto de la lateralidad debe ser l o r.',
+            'caracteristic.required' => 'Este campo es requerido.',
+            'caracteristic.in' => 'Debes seleccionar 0, F o P.',
+            'comments.string' => 'El comentario debe ser una cadena de texto.',
+            'comments.required_if' => 'El comentario es obligatorio.',
+            'laterality_impact.required_if' => 'Este campo es requerido.',
+            'laterality_impact.in' => 'Debe seleccionar el Hemisferio del impacto.',
             'brain_level_id.required' => 'El nivel cerebral es requerido.',
             'brain_level_id.exists' => 'El nivel cerebral seleccionado no existe.',
             'brain_function_id.required' => 'La función cerebral es requerida.',
