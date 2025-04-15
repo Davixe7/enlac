232d5551d0438c19c17714261ff4867b8f5c812b
David <davidguilarte7@gmail.com>
2025-04-15 02:06:49 -0400

    stable

--- database/migrations/2025_04_12_132934_create_payments_table.php
+++ database/migrations/2025_04_12_132934_create_payments_table.php
@@ -13,11 +13,16 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
-            $table->date('date');
-            $table->foreignId('sponsor_id')->constrained()->onDelete('cascade');
             $table->foreignId('candidate_id')->constrained()->onDelete('cascade');
+            $table->foreignId('sponsor_id')->constrained()->onDelete('cascade');
+            $table->enum('payment_type', ['parent', 'sponsor'])->default('parent');
+            $table->boolean('is_partial')->default(false);
+            $table->date('date');
+            $table->string('payment_method');
+            $table->string('ref')->nullable();
+            $table->text('comments')->nullable();
             $table->decimal('amount');
+            $table->timestamps();
         });
     }
 
