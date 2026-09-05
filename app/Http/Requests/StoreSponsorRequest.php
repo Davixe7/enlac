4759b3ab6f589a5cd8113c707aba52c237b5c8b2
David <davidguilarte7@gmail.com>
2026-09-05 03:57:17 -0400

    (sponsorRequest) agregada la validacion que requiere tipo de sponsor solo si se esta creando

--- app/Http/Requests/StoreSponsorRequest.php
+++ app/Http/Requests/StoreSponsorRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreSponsorRequest extends FormRequest
 {
@@ -21,6 +22,8 @@ class StoreSponsorRequest extends FormRequest
      */
     public function rules(): array
     {
+        $isUpdate = $this->isMethod('put') || $this->isMethod('patch') || $this->route('sponsor');
+
         return [
             'name'             => 'required|string|max:191',
             'last_name'        => 'required|string|max:191',
@@ -32,6 +35,14 @@ class StoreSponsorRequest extends FormRequest
             'contact_by'       => 'required',
             'is_anonymous'     => 'required|boolean',
             'type'             => 'required',
+
+            'candidate_id' => [
+                Rule::requiredIf(function () use ($isUpdate) {
+                    return $this->type === 'link' && !$isUpdate;
+                }),
+                'nullable',
+                'exists:candidates,id',
+            ],
 
             'addresses.*.street'       => 'nullable',
             'addresses.*.inner_number' => 'nullable|string|max:50',
